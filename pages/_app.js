8fd867ef8aa4e2780a6f6d7baf005a448ee11b43
myenji <enjisecond559@gmail.com>
2026-02-18 23:18:22 +0700

    Add SessionProvider to _app.js for session management

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import { SessionProvider } from "next-auth/react";
+export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+}
